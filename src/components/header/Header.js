7efafdfd3e44d288b4d3c9697f036a71b73f1b42
DevBrian <DevBrian@Brians-MacBook-Pro.local>
2017-06-21 17:03:39 -0600

    added navbar links and styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './Header.css';
+
 
 class Header extends Component {
     render() {
         return (
             <div>
-                Header
+              <ul className="navbar">
+                <li><Link to="/">HOME</Link></li>
+                <li><Link to="/about">ABOUT</Link></li>
+                <li>LOGIN/REGISTER</li>
+              </ul>
             </div>
         );
     }
