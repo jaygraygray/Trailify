f5b3acf3e229bbff0c035dcad413a0480e386403
Chase Derr <Chisnits@users.noreply.github.com>
2017-06-20 21:53:29 -0600

    added routing with Hash history. Created components Header/Footer that are always displayed throughout the site.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './redux/store'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
+ReactDOM.render(
+ <Provider store={store}>
+    <Router>
+      <div>
+        <Route component={ Header }/>
+        <Route exact path="/" component={ App }/>
+        <Route component={ Footer }/>
+      </div>
+    </Router>
+  </Provider>, 
+  document.getElementById('root'));
 registerServiceWorker();
