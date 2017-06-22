935bef2524c29645d0a43eaff1709df781c68d1f
DevBrian <DevBrian@Brians-MacBook-Pro.local>
2017-06-22 14:21:18 -0600

    added server folder, config.js and index.js. setup index.js file.

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  massiveConnection: {
+    connectionString: 'postgres://ydhvtnzsgkgrok:d1a62e318ef652a306073f1e386a27fd43de8fc073f273a54d74e5c81be2dcd3@ec2-107-20-255-96.compute-1.amazonaws.com:5432/d9mg7rti7nvokv?ssl=true'
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,40 @@
+//===================== Required Dependencies =======================//
+
+const express = require('express')
+      , bodyParser = require('body-parser')
+      , massive = require('massive')
+      , session = require('session')
+      , cors = require('cors')
+      , passport = require('passport')
+      , Auth0Strategy = require('passport-auth0')
+      , config = require('./config')
+
+//========================= Intialize App ===========================//
+
+const app = module.exports = express()
+app.use(bodyParser.json())
+app.use(cors())
+app.use(express.static('public'))
+
+//=========================== Database ==============================//
+
+const massiveInstance = massive.connectSync(config.massiveConnection)
+app.set('db', massiveInstance)
+const db = app.get('db')
+
+//========================== Controller =============================//
+
+// const productsCtrl = require('./controllers/productsCtrl')
+
+//========================= Get Request =============================//
+
+// app.get('/api/products', productsCtrl.getproducts)
+// app.get('/api/hats', productsCtrl.gethats)
+
+//======================= Listening Port ============================//
+
+const port = 8000
+
+app.listen(port, () => {
+  console.log("Started server on port", port)
+});
