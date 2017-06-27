978cce9488fd6d0ba7e0eda8e1ed739e4700428c
Taylor Griffith <tgriffith14@hotmail.com>
2017-06-27 12:39:46 -0600

    results page done

--- src/components/TrailResults/TrailResults.js
+++ src/components/TrailResults/TrailResults.js
@@ -19,8 +19,8 @@ class TrailResults extends Component {
 
       const TrailData = this.props.info.map((data, i) => (
           <div className="trail-list-items" key={i}>
-            <Link to={`/details/${data.unique_id}`}>
-            <h3 id="list-name">{data.name}</h3>
+            <Link id="results-link" to={`/details/${data.unique_id}`}>
+            <h2 id="list-name">{data.name}</h2>
             <img src={data.activities[0].thumbnail} alt="Photo Not Found" />
             <h4 id="list-rating">Rating: {data.activities[0].rating > 0 ? data.activities[0].rating + "/5" : "N/A"}</h4>
             <h4 id="list-length">Length: {data.activities[0].length > 0 ? data.activities[0].length + " mi" : "N/A"}</h4>
