7f8835a3b13baae8a58b6c0df4726541344ef71e
davidtagholmes <David@Sams-MacBook-Pro.local>
2017-06-30 16:51:25 -0600

    style-changes

--- src/components/LandingPage/Featured/Hexagon.js
+++ src/components/LandingPage/Featured/Hexagon.js
@@ -16,7 +16,6 @@ this.setState({
 
     render() {
         return (
-<<<<<<< HEAD
           <div>
               <div className="hexagon">
                   <img className="hexagon-background-image" src={this.state.trail.photo_1} />
@@ -25,14 +24,6 @@ this.setState({
                   </div>
               </div>
           </div>
-=======
-            <div>
-                <div className="hexagon">
-                    <img className="hexagon-background-image" src={this.state.trail.photo_1} alt="trail pic"/>
-                    <h1>{this.state.trail.featured_trail_name}</h1>
-                </div>
-            </div>
->>>>>>> master
         );
     }
 }
