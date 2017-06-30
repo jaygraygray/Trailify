d268a1b5776513f57fcbd8ccfe780702a82e7284
Chase Derr <Chisnits@users.noreply.github.com>
2017-06-30 14:59:38 -0600

    fixed google map bug

--- src/components/TrailResults/GoogleMap.js
+++ src/components/TrailResults/GoogleMap.js
@@ -1,14 +1,6 @@
 /* global google */
 import { default as React, Component, } from "react";
-
 class Map extends Component {
-    componentWillReceiveProps(nextProps) {
-    this.setState({
-      lat: nextProps.center.lat,
-      lng: nextProps.center.lng,
-    });
-  }
-
   componentDidMount(){
     new google.maps.Map(document.getElementById("map"),{
       center: {lat: "", lng: ""},
@@ -21,13 +13,15 @@ class Map extends Component {
     new google.maps.Map(myMap,{
       center: { lat:this.props.center.lat, lng: this.props.center.lng },
       zoom: 8
+  })
   }
-      )
-  }
+  
   render() {
     return (
       <div id="map">
+         
       </div>
+  
     );
   }
 }
