08c44536289835c22d23fb183eda41dbe45d6ff5
Chase Derr <Chisnits@users.noreply.github.com>
2017-07-03 14:39:17 -0600

    setup google map

--- src/components/LandingPage/Nearby/HomeMap.js
+++ src/components/LandingPage/Nearby/HomeMap.js
@@ -1,10 +1,22 @@
+/*global google*/
 import React, { Component } from 'react';
 
+
 class HomeMap extends Component {
+
+    componentDidMount(){
+        new google.maps.Map(document.getElementById("homeMap"),{
+        center: {lat: 40.233845, lng: -111.658531},
+        zoom: 8
+        })
+    }
+    
     render() {
         return (
-            <div>
-                Map
+            <div className="wrapper">
+            <div id="homeMap">
+                
+            </div>
             </div>
         );
     }
