27da3853af1fd6c39cb39c59cda3d40a3f121bbd
DevBrian <briandinhtran@outlook.com>
2017-07-03 10:36:06 -0600

    removed consolelog

--- src/components/Footer/Newsletter/Newsletter.js
+++ src/components/Footer/Newsletter/Newsletter.js
@@ -16,20 +16,15 @@ class Newsletter extends Component {
   }
 
   handleEmail(event) {
-    console.log(event.target.value);
     this.setState({email: event.target.value})
   }
 
   handleSubmit(event) {
     event.preventDefault()
 
-    axios.post('/contactus', {
-      email: this.state.email
-    })
-    .then(function (response) {
+    axios.post('/contactus', {email: this.state.email}).then(function(response) {
       console.log(response);
-    })
-    .catch(function (error) {
+    }).catch(function(error) {
       console.log(error);
     });
 
@@ -39,16 +34,9 @@ class Newsletter extends Component {
       <div >
         <h2 className="footer-headline">Newsletter</h2>
         <h3 className="newsletter-headline">Subscribe to our newsletter to stay up to date!</h3>
-        <form onSubmit={this.handleSubmit} >
-          <input
-            id="email-input"
-            type="email"
-            placeholder="E-MAIL"
-            value={this.state.email}
-            onChange={this.handleEmail} required/>
-          <input
-            id="email-submit"
-            type="submit" />
+        <form onSubmit={this.handleSubmit}>
+          <input id="email-input" type="email" placeholder="E-MAIL" value={this.state.email} onChange={this.handleEmail} required/>
+          <input id="email-submit" type="submit"/>
         </form>
       </div>
     );
